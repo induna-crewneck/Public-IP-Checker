944de03b76bde124573a39a94e160288bd96d48b
induna-crewneck <66157637+induna-crewneck@users.noreply.github.com>
2024-11-29 19:13:58 +0100

    Update check_ips.py to v2
    Changelog for v2:
    - Removed unused function
    - Not storing IP location (in case getting location fails). Only checking before logging/notifying
    - icanhazip as fallback if ipify fails
    - More detailled error handling and logging
    - Added header

--- check_ips.py
+++ check_ips.py
@@ -1,7 +1,21 @@
+'''
+https://github.com/induna-crewneck/Public-IP-Checker
+v2
+
+Changelog:
+- Removed unused function
+- Not storing IP location (in case getting location fails). Only checking before logging/notifying
+- icanhazip as fallback if ipify fails
+- More detailled error handling and logging
+- Added header
+To Do:
+'''
+
 import os
 import time
 import requests
 import subprocess
+import re
 from datetime import datetime
 
 # Load environment variables
@@ -12,6 +26,8 @@ TELEGRAM_RECEIVER = os.getenv("TELEGRAM_RECEIVER")
 
 log_file = "/app/public-ips.log"
 ips_file = "/app/public-ips.txt"
+
+ipv4_regex = r"^\d*\.\d*\.\d*\.\d*$"
 
 def log_message(message):
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -43,18 +59,24 @@ def get_ip_location(ip):
 
 def get_public_ip():
     try:
-        return requests.get("https://api.ipify.org").text
+        public_ip = requests.get("https://api.ipify.org")
+        if re.fullmatch(ipv4_regex,public_ip.text):
+            return public_ip.text
+        else:
+            log_message(f"ipify did not return valid public IP: {public_ip}")
     except Exception as e:
-        log_message(f"Failed to get public IP: {e}")
-        return str(e)
-
-def get_router_ip():
-    # unused, because public router and public device IP are the same
+        log_message(f"Exception getting public IP from ipify: {e}")
     try:
-        return requests.get("https://api.ipify.org", proxies={"http": "http://192.168.2.1"}).text
+        public_ip = requests.get("https://ipv4.icanhazip.com")
+        if re.fullmatch(ipv4_regex, public_ip.text.strip()):
+            log_message(f"  icanhazip fallback successfull")
+            return public_ip.text.strip()
+        else:
+            log_message(f"icanhazip did not return valid public IP: {public_ip}")
+        return "failled"
     except Exception as e:
-        log_message(f"Failed to get router IP: {e}")
-        return str(e)
+        log_message(f"Exception getting public IP from icanhazip: {e}")
+        return "failed"
 
 def get_local_ip():
     try:
@@ -79,20 +101,50 @@ def get_local_ip():
 
 def get_docker_ip(container_name):
     try:
+        # Try fetching IP using ipify
         result = subprocess.run(
             ["docker", "exec", container_name, "curl", "-s", "https://api.ipify.org"],
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
         )
         if result.returncode == 0:
-            ip = result.stdout.decode().strip()
-            ip_location = get_ip_location(ip)
-            return f"{ip} ({ip_location})"
+            docker_ip = result.stdout.decode().strip()
+            if "connection" not in docker_ip and re.match(ipv4_regex, docker_ip): # Validate it's a valid IPv4
+                return docker_ip
+            else:
+                log_message(f"ipify did not return valid {DOCKER_CONTAINER} IP (1): {docker_ip}")
+        elif result.returncode == 6:
+            log_message(f"ipify did not return valid {DOCKER_CONTAINER} IP with curl returncode 6: Could not resolve host")
+        elif result.returncode == 7:
+            log_message(f"ipify did not return valid {DOCKER_CONTAINER} IP with curl returncode 6: Could not connect (server or host down)")
+        elif result.returncode == 28:
+            log_message(f"ipify did not return valid {DOCKER_CONTAINER} IP with curl returncode 28: Operation Timeout")
+        elif len(result.stderr.decode().strip()) > 0:
+            log_message(f"ipify did not return valid {DOCKER_CONTAINER} IP (2): {result.stderr.decode().strip()}")
         else:
-            raise Exception(result.stderr.decode().strip())
+            log_message(f"ipify did not return valid {DOCKER_CONTAINER} IP (2): {result}")
     except Exception as e:
-        log_message(f"Failed to get IP for container {container_name}: {e}")
-        return str(e)
+        log_message(f"Exception getting {DOCKER_CONTAINER} IP from ipify: {e}")
+    try:
+        # Fallback to icanhazip.com
+        result_fallback = subprocess.run(
+            ["docker", "exec", container_name, "curl", "-s", "https://ipv4.icanhazip.com"],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+        if result_fallback.returncode == 0:
+            docker_ip = result_fallback.stdout.decode().strip()
+            if "connection" not in docker_ip and re.match(ipv4_regex, docker_ip):  # Validate it's a valid IPv4
+                log_message(f"  icanhazip fallback successfull")
+                return docker_ip
+            else:
+                log_message(f"icanhazip did not return valid {DOCKER_CONTAINER} IP (1): {docker_ip}")
+        else:
+            log_message(f"icanhazip did not return valid {DOCKER_CONTAINER} IP (2): {public_ip}")
+    except Exception as e:
+        log_message(f"Exception getting {DOCKER_CONTAINER} IP from icanhazip: {e}")
+        return "failed"
+
 
 def send_telegram_message(message):
     try:
@@ -121,7 +173,11 @@ def initial_messaging():
     # Log initial IPs
     try:
         for key, value in initial_ips.items():
-            log_message(f"Initial {key} IP: {value}")
+            if key == DOCKER_CONTAINER:
+                location = get_ip_location(value)
+                log_message(f"Initial {key} IP: {value} ({location})")
+            else:
+                log_message(f"Initial {key} IP: {value}")
     except Exception as e:
         log_message(f"Failed to log initial IPs: {e}")
     # Write initial IPs
@@ -135,7 +191,11 @@ def initial_messaging():
     try:
         initialmessage = "Watchdog started\n\n"
         for key, value in initial_ips.items():
-            initialmessage += f"{key} IP: {value}\n"
+            if key == DOCKER_CONTAINER:
+                location = get_ip_location(value)
+                initialmessage += f"{key} IP: {value} ({location})\n"
+            else:
+                initialmessage += f"{key} IP: {value}\n"
         send_telegram_message(initialmessage)
     except Exception as e:
         log_message(f"Failed to send initial Telegram message: {e}")
@@ -144,6 +204,7 @@ def initial_messaging():
     return initial_ips
 
 def main():
+    print(f"{'='*64}\n{'='*23} Public IP Checker {'='*22}\n{'='*5} https://github.com/induna-crewneck/Public-IP-Checker {'='*5}\n{'='*64}", end="\n")
     global previous_ips
     previous_ips = {"local": "", "public": "", "router": "", f"{DOCKER_CONTAINER}": ""}
 
@@ -159,7 +220,7 @@ def main():
 
     while True:
         time.sleep(CHECK_INTERVAL)
-        
+
         current_ips = fetch_ips()
 
         # Check for changes
